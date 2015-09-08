2d9aaf0ec2cd85caa414eea5d3c494f6d8e06fcf
Jason <jason.arita@gmail.com>
2015-09-08 11:24:14 -0700

    Scraped, Cleaned, Merged Data + some exploration

--- fantasy_football_analysis.R
+++ fantasy_football_analysis.R
@@ -21,8 +21,6 @@ url <- paste0("http://sports.yahoo.com/nfl/stats/byteam?group=Offense&cat=Total&
 offense <- readHTMLTable(url, encoding = "UTF-8", colClasses = "character")[[7]]
 
 
-
-
 # Clean Offense data ------------------------------------------------------
 
 # Delete blank columns from raw dataset
@@ -40,24 +38,70 @@ offense[,14] <- as.numeric(substr(offense[,14], 1, 2))*60
 
 
 # Scrape Defense data -----------------------------------------------------
-url <- paste0("http://sports.yahoo.com/nfl/stats/byteam?group=Defense&cat=Total&conference=NFL&year=season_" 
+url     <- paste0("http://sports.yahoo.com/nfl/stats/byteam?group=Defense&cat=Total&conference=NFL&year=season_" 
               , year 
               , "&sort=530&old_category=Total&old_group=Defense")
-
 defense <- readHTMLTable(url, encoding = "UTF-8", colClasses = "character")[[7]]
 
 
-
 # Clean Defense data ------------------------------------------------------
-
-# Delete blank columns from raw dataset
-defense <- defense[,-seq(2,length(defense), 2)]
-
-
-# Force all team names into char strings
-defense[,1] <- as.character(defense[,1])
-
-# Convert all stats to numerics
-defense[,2:13] <- apply(defense[,2:length(defense)], 2, as.numeric)
+defense           <- defense[,-seq(2,nVars, 2)]
+nVars             <- length(defense)
+defense[,1]       <- as.character(defense[,1])
+defense[,2:nVars] <- apply(defense[,2:nVars], 2, as.numeric)
 
 
+
+# Merge the Offense and Defense Data --------------------------------------
+
+combined_df <- merge(offense, defense, by.x = "Team", by.y = "Team")
+
+# Clean data --------------------------------------------------------------
+
+colnames(combined_df)[3]  <- "OffPPG"
+colnames(combined_df)[2]  <- "Games"
+colnames(combined_df)[3]  <- "OffPPG"
+colnames(combined_df)[4]  <- "OffYPG"
+colnames(combined_df)[5]  <- "OffPassYPG"
+colnames(combined_df)[6]  <- "OffRushYPG"
+combined_df$G.y           <- NULL
+colnames(combined_df)[15] <- "DefPPG"
+colnames(combined_df)[16] <- "DefYPG"
+colnames(combined_df)[17] <- "DefRushYPG"
+colnames(combined_df)[18] <- "DefPassYPG"
+
+
+
+# Data exploration: Histograms --------------------------------------------
+
+hist(combined_df$OffPPG
+     , breaks = 50
+     , main = "Offensive Points Per Game"
+     , xlab = "Offensive PPG"
+     , ylab = "Number of Teams" 
+     , col = "blue")
+
+mean(combined_df$OffPPG)
+sd(combined_df$OffPPG)
+max(combined_df$OffPPG)
+min(combined_df$OffPPG)
+
+# Defensive Points Per Game
+hist(combined_df$DefPPG
+, breaks = 10
+, main = "Defensive Points Per Game"
+, xlab = "Defensive PPG"
+, ylab = "Number of Teams"
+, col = "red")
+
+mean(combined_df$DefPPG)
+sd(combined_df$DefPPG)
+
+# Offensive first downs per game
+hist(combined_df$`1stD/G`
+, breaks = 10
+, main = "Offensive 1st downs Per Game"
+, xlab = "1st Downs Per Game"
+, ylab = "Number of Teams"
+, col = "green")
+)
