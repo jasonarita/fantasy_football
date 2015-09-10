390b7b0936acad86e5d3dde78fb7dbfca53c41fe
jasonarita <jtkarita-admin@percpt.local>
2015-09-09 17:21:44 -0700

    Bar charts + Scatterplots + started Indexes

--- fantasy_football_analysis.R
+++ fantasy_football_analysis.R
@@ -75,11 +75,17 @@ colnames(combined_df)[18] <- "DefPassYPG"
 # Data exploration: Histograms --------------------------------------------
 
 hist(combined_df$OffPPG
-     , breaks = 50
+     , breaks = 10
      , main = "Offensive Points Per Game"
      , xlab = "Offensive PPG"
      , ylab = "Number of Teams" 
      , col = "blue")
+
+# GGPLOT Version
+# ggplot(combined_df, aes(x=OffPPG)) +
+#   geom_histogram(aes(y=..density..), binwidth=2, color = "black", fill = "blue") +
+#   geom_density()
+
 
 mean(combined_df$OffPPG)
 sd(combined_df$OffPPG)
@@ -96,6 +102,8 @@ hist(combined_df$DefPPG
 
 mean(combined_df$DefPPG)
 sd(combined_df$DefPPG)
+max(combined_df$DefPPG)
+min(combined_df$DefPPG)
 
 # Offensive first downs per game
 hist(combined_df$`1stD/G`
@@ -104,4 +112,96 @@ hist(combined_df$`1stD/G`
 , xlab = "1st Downs Per Game"
 , ylab = "Number of Teams"
 , col = "green")
-)
+rug(jitter(combined_df$`1stD/G`, amount = 0.01)
+    , col = "green"
+    , lwd = 2.0)
+
+
+
+
+# Bar Charts: How different teams compare ---------------------------------
+
+ppg <- transform(combined_df, Team=reorder(Team, combined_df$OffPPG))
+ggplot(ppg, aes(x=Team, y=OffPPG)) + 
+  geom_bar(stat="identity", color="black", fill="blue") + 
+  coord_flip() + 
+  labs(x="Team", y="Avg Points Per Game") + 
+  ggtitle("Avg Points Per Game") + 
+  theme(plot.title=element_text(size=18, face="bold"))
+
+
+maxYards = 500
+
+# Defensive YPG
+defYPG <- transform(combined_df, Team=reorder(Team, combined_df$DefYPG))
+ggplot(defYPG, aes(x=Team, y=DefYPG)) + 
+  geom_bar(stat="identity", color="black", fill="red") + 
+  ylim(0, maxYards) +
+  coord_flip() +
+  labs(x="Team", y="Avg Yards Allowed Per Game") +
+  ggtitle(paste(year, "- Avg yards ALLOWED Per Game")) +
+  theme(plot.title=element_text(size=18, face="bold"))
+  
+# Defensive Pass YPG
+defPassYPG <- transform(combined_df, Team=reorder(Team, combined_df$DefPassYPG))
+
+ggplot(defPassYPG, aes(x=Team, y=DefPassYPG)) +
+  geom_bar(stat="identity", color="black", fill="firebrick4") +
+  ylim(0, maxYards) +
+  coord_flip() +
+  labs(x="Team", y="Avg Pass Yards Allowed Per Game") +
+  ggtitle(paste(year, "- Avg Pass Yards Allowed Per Game")) +
+  theme(plot.title=element_text(size=18, face="bold"))
+
+
+
+
+# Scatterplots: Relationships/correlations between variables --------------
+
+
+# Offensive YPG vs PPG
+ggplot(combined_df, aes(x=combined_df$OffYPG, y=combined_df$OffPPG, label=combined_df$Team)) +
+  geom_point(shape=5, size=2, color="blue") +
+  geom_text(aes(label=Team), hjust=1.10, vjust=0.45) +
+  geom_smooth() +
+  labs(x="Yards Per Game", y="Points Scored Per Game") +
+  ggtitle("Offensive Yards per Game vs Points per Game") +
+  theme(plot.title=element_text(size=18, face="bold"))
+
+cor(combined_df$OffPPG, combined_df$OffYPG)
+
+# Defensive YPG vs PPG
+ggplot(combined_df, aes(x=combined_df$DefYPG, y=combined_df$DefPPG, label=combined_df$Team)) +
+  geom_point(shape=5, size=2, color="firebrick1") +
+  geom_text(aes(label=Team), hjust=.50, vjust=-1.45) +
+  geom_smooth() +
+  labs(x="Yards Per Game", y="Points Allowed Per Game") +
+  ggtitle("Defensive Yards per Game vs Points per Game") +
+  theme(plot.title=element_text(size=18, face="bold"))
+
+cor(combined_df$DefPPG, combined_df$DefYPG)
+
+# Time of Posession (TOP) vs PPG
+ggplot(combined_df, aes(x=combined_df$TOP, y=combined_df$OffPPG, label=combined_df$Team)) +
+  geom_point(shape=5, size=2, color="magenta") +
+  geom_text(aes(label=Team), hjust=.50, vjust=-1.45) +
+  geom_smooth() +
+  labs(x="Time of Possession (Seconds)", y="Points Scored Per Game") +
+  ggtitle("Time of Possession vs Points per Game") +
+  theme(plot.title=element_text(size=18, face="bold"))
+
+cor(combined_df$TOP, combined_df$OffPPG)
+
+
+
+
+# Indexes -----------------------------------------------------------------
+
+combined_df$OffPassStrength <-  max(combined_df$OffPassYPG) - combined_df$OffYPG
+combined_df$OffPassStrength <- (1 - (combined_df$OffPassStrength/max(combined_df$OffPassStrength))) * 100
+
+# Z-score
+combined_df$OffPassStrength <- combined_df$OffYPG - mean(combined_df$OffPassYPG)
+combined_df$OffPassStrength <- combined_df$OffPassStrength/sd(combined_df$OffPassYPG)
+
+hist(x=combined_df$OffPassStrength)
